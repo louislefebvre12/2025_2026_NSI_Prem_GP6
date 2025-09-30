94a0bcfc0a7f0ffcf0b758416e90e3316bc9bd2e
alex-jga <jaouena8@gmail.com>
2025-09-30 17:09:04 +0200

    Update Retrait.py

--- Retrait.py
+++ Retrait.py
@@ -3,7 +3,11 @@
 #si retrait fait:
 #- retirer le montant au solde + noter la date dans un historique
 
-message_retrait = "Choisissez un montant "
-montant_retrait = input(message_retrait)
+message_billetsde200 = "Séléctionnez le nombre de billets de 200EU : "
+message_billetsde100 = "Séléctionnez le nombre de billets de 100EU : "
+message_billetsde50 = "Séléctionnez le nombre de billets de 50EU : "
+message_billetsde10 = "Séléctionnez le nombre de billets de 10EU : "
+message_billetsde5 = "Séléctionnez le nombre de billets de 5EU : "
 
-print(montant_retrait)
+montant_retrait = input(message_billetsde200) + input(message_billetsde100) + input(message_billetsde50) + input(message_billetsde10) + input(message_billetsde5) 
+print(f"Le montant total retiré est de {montant_retrait} EU")
