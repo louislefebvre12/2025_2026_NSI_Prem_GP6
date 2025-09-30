fd520ac35864cb416b77c4a6bbf76dd379f3b34f
louislefebvre12 <louislefebvre12@icloud.com>
2025-09-30 17:10:15 +0200

    ajout depot

---
+++ Depot.py
@@ -0,0 +1,9 @@
+#depot:
+#- rentrer un montant 
+#si depot fait: 
+#- ajouter le montant au solde + noter la date dans un historique 
+
+message_depot = "Choisissez un montant "
+montant_depot = input(message_depot)
+
+print(montant_depot)
