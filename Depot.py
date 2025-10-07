56b5fa641a15bdd9213605ff07a151714c092bc7
louislefebvre12 <louislefebvre12@icloud.com>
2025-10-07 17:10:52 +0200

    depot + data + main

--- Depot.py
+++ Depot.py
@@ -2,8 +2,43 @@
 #- rentrer un montant 
 #si depot fait: 
 #- ajouter le montant au solde + noter la date dans un historique 
+import data
+from data import *
 
-message_depot = "Choisissez un montant "
-montant_depot = input(message_depot)
 
-print(montant_depot)
+
+
+
+
+# reponse_message_depot_verif 
+
+def rep_is_yes (rep):
+    return rep in yes_responses
+
+def rep_is_no (rep):
+    return not (rep_is_yes (rep)) 
+
+def add_amount_to_account ():
+    pass
+
+def make_a_depot ():
+    pass
+
+if rep_is_yes (rep):
+    add_amount_to_account ()
+
+def propose_depot ():
+    message_depot = "Choisissez un montant "
+    montant_depot = input(message_depot)
+    message_depot_verif = f"Etes-vous sur de vouloir déposer cette somme: {montant_depot}€ ? " 
+    return montant_depot
+
+def run ():
+    rep = input(message_depot_verif) 
+    while rep_is_yes (rep):
+        make_a_depot ()
+    propose_depot ()
+
+
+
+run ()
