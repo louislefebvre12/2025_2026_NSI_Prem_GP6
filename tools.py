a0557825aa7f2a92b788d3a0997ddbfdcf129ea5
axile09 <vialaaxel@gmail.com>
2025-10-09 11:49:17 +0200

    Update tools.py

--- tools.py
+++ tools.py
@@ -1,3 +1,4 @@
+<<<<<<< Updated upstream
 
 
 
@@ -30,6 +31,31 @@
 
 
 #depot:
+=======
+# entrer dans le transfert 
+# sélectionner le client à qui envoyer l'argent
+# sélectionner le montant à envoyer
+# afficher le message de verification de notre action
+# retirer la somme du compte du premier client
+# ajouter la meme somme au deuxieme client
+
+while client_is_in_transfert_page ():
+    show transfert_message
+    ask_client1_select_client2 ()
+    
+
+    #retrait:
+#- choisisser un montant 
+#si retrait fait:
+#- retirer le montant au solde + noter la date dans un historique
+
+monant = input("Choisissez un montant : ")
+message_grosses_coupures : input("Voulez vous des grosses coupures? ")
+def message_grosses_coupures() : 
+    if 
+
+    #depot:
+>>>>>>> Stashed changes
 #- rentrer un montant 
 #si depot fait: 
 #- ajouter le montant au solde + noter la date dans un historique 
@@ -38,9 +64,12 @@ from data import *
 
 
 
+<<<<<<< Updated upstream
 
 
 
+=======
+>>>>>>> Stashed changes
 # reponse_message_depot_verif 
 
 def rep_is_yes (rep):
@@ -49,8 +78,12 @@ def rep_is_yes (rep):
 def rep_is_no (rep):
     return not (rep_is_yes (rep)) 
 
+<<<<<<< Updated upstream
 def save_depot_in_historique ():
     pass
+=======
+
+>>>>>>> Stashed changes
 
 def propose_depot ():
     message_depot = "Choisissez un montant "
@@ -58,18 +91,26 @@ def propose_depot ():
     return montant_depot
 
 def add_amount_to_account ():
+<<<<<<< Updated upstream
     montant_depot () 
+=======
+    montant_depot 
+>>>>>>> Stashed changes
 
 def fonctionnement_depot ():
     propose_depot ()
     print(message_depot_verif) 
     while rep_is_yes :
         add_amount_to_account
+<<<<<<< Updated upstream
         save_depot_in_historique
+=======
+>>>>>>> Stashed changes
         return return_to_accueil
     else :
         rep_is_no
         propose_depot
+<<<<<<< Updated upstream
     
 
 
@@ -80,3 +121,6 @@ def fonctionnement_depot ():
 
 
 fonctionnement_depot ()
+=======
+    
+>>>>>>> Stashed changes
