282dfa25ea8a5677154229315b09dd4498bb2ad2
axile09 <vialaaxel@gmail.com>
2025-10-09 11:38:18 +0200

    reorganisation complète

--- Retrait.py
+++
@@ -1,9 +0,0 @@
-#retrait:
-#- choisisser un montant 
-#si retrait fait:
-#- retirer le montant au solde + noter la date dans un historique
-
-monant = input("Choisissez un montant : ")
-message_grosses_coupures : input("Voulez vous des grosses coupures? ")
-def message_grosses_coupures() : 
-    if 
