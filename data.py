4b24e5511f667d574a096e0963ece35a5c6f7cc4
louislefebvre12 <louislefebvre12@icloud.com>
2025-10-14 16:19:07 +0200

    data client idée

--- data.py
+++ data.py
@@ -4,6 +4,17 @@ quitting_words = ["ciao","bye","aurevoir","salut","quit"]
 def return_to_accueil ():
     pass
 
+import datetime
+
+save_depot_in_historique = datetime.datetime.now()
+print(save_depot_in_historique) 
+
+save_withdraw_in_historique = datetime.datetime.now()
+print(save_withdraw_in_historique) 
+
+save_transfert_in_historique = datetime.datetime.now()
+print(save_transfert_in_historique) 
+
 
 
 
@@ -14,3 +25,15 @@ def return_to_accueil ():
 #Pour depot:
 
 message_depot_verif = f"Etes-vous sur de vouloir déposer cette somme: {montant_depot}€ ? "
+
+
+
+
+#clients
+
+client_001_id = 001 : { "nom" : "Kaltrachian"/
+                      "prenom" : "Bogdan" /
+                      "solde" : "346" / 
+                      "historique_depot" : "[206,"save_depot_in_historique"]" /
+                      "historique_retrait" :"[12,"save_withdraw_in_historique"]" /
+                      "historique_transfert" : "[34,id_client_recepteur,"save_transfert_in_historique"]"}
