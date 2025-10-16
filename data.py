4707d53c74004de8af3ef63e57a626402857c157
louislefebvre12 <louislefebvre12@icloud.com>
2025-10-16 12:09:33 +0200

    update de data

--- data.py
+++ data.py
@@ -35,5 +35,26 @@ client_001_id = 001 : { "nom" : "Kaltrachian"/
                       "prenom" : "Bogdan" /
                       "solde" : "346" / 
                       "historique_depot" : "[206,"save_depot_in_historique"]" /
-                      "historique_retrait" :"[12,"save_withdraw_in_historique"]" /
+                      "historique_retrait" :"[20,"save_withdraw_in_historique"]" /
                       "historique_transfert" : "[34,id_client_recepteur,"save_transfert_in_historique"]"}
+
+client_001_id = 002 : { "nom" : "Viara"/
+                      "prenom" : "Axel" /
+                      "solde" : "3" / 
+                      "historique_depot" : "[4,"save_depot_in_historique"]" /
+                      "historique_retrait" :"[10,"save_withdraw_in_historique"]" /
+                      "historique_transfert" : "[2200,id_client_recepteur,"save_transfert_in_historique"]"}
+
+client_001_id = 003 : { "nom" : "Jaouen Garcia"/
+                      "prenom" : "Alexandre" /
+                      "solde" : "67000" / 
+                      "historique_depot" : "[267,"save_depot_in_historique"]" /
+                      "historique_retrait" :"[70,"save_withdraw_in_historique"]" /
+                      "historique_transfert" : "[567,id_client_recepteur,"save_transfert_in_historique"]"}
+
+client_001_id = 004 : { "nom" : "Lefebvre"/
+                      "prenom" : "Louis" /
+                      "solde" : "90210" / 
+                      "historique_depot" : "[3500,"save_depot_in_historique"]" /
+                      "historique_retrait" :"[60,"save_withdraw_in_historique"]" /
+                      "historique_transfert" : "[12,id_client_recepteur,"save_transfert_in_historique"]"}
